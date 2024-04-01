623c1afb033323995ac7e842ea5bf598c1d0e7a7
magangdesigner <161426152+magangdesigner@users.noreply.github.com>
2024-04-01 22:59:13 +0700

    revisi

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 const mysql = requere("mysql");
 
 const db = mysql.createConnection({
-    host : "",
-    user : "",
-    database : "",
-    password : "",
+    host : "sql6.freesqldatabase.com",
+    user : "sql6695840",
+    database : "sql6695840",
+    password : "EM2kv1mBfv",
 });
 
 module.exports = db;
