b7f1cfca571e25c56c71415529522525c9776f61
magangdesigner <161426152+magangdesigner@users.noreply.github.com>
2024-04-29 16:45:08 +0700

    add konfig vercel.json

--- connection.js
+++ connection.js
@@ -2,9 +2,9 @@ const mysql = requere("mysql");
 
 const db = mysql.createConnection({
     host : "sql6.freesqldatabase.com",
-    user : "sql6695840",
-    database : "sql6695840",
-    password : "EM2kv1mBfv",
+    user : "sql6702772",
+    database : "sql6702772",
+    password : "z1y63UXxGk",
 });
 
 module.exports = db;
